bc0ffca76e4c4084487eaa6ef6c92f353e02f8de
hsinyingchen <43963801+hsinyingchen@users.noreply.github.com>
2022-09-17 21:08:10 +0800

    控制系統

---
+++ Hunter/Assets/Script/ControlSystem.cs
@@ -0,0 +1,24 @@
+
+using UnityEngine;
+
+namespace Chen
+{
+    public class ControlSystem : MonoBehaviour
+    {
+        private float speedRotation = 10f;
+        private int numMarble = 10;
+        private int speedMarble = 1500;
+        private float interlMarble = 0.5f;
+        private GameObject prefabMarble;
+        private string animationattackParm = "Attack";
+
+
+
+
+
+        
+
+
+    }
+}
+
--- Hunter/Assets/Script/LearnField.cs
+++ Hunter/Assets/Script/LearnField.cs
@@ -10,6 +10,70 @@ namespace Chen
  
     public class LearnField : MonoBehaviour
     {
+        //欄位語法:
+        //修飾詞 資料類型 欄位名稱;
+        private int number;
+        //修飾詞 資料類型 欄位名稱;
+        private int level = 1;
+        //私人:此類別可以存取 不會顯示在屬性面板
+        private int scoreA = 60;
+        //公開:所有類別可以存取 會顯示在屬性面板
+        //整數 int
+        public int scoreB = 90;
+        //浮點數 float
+        public float speed = 3.5f;
+        //字串 string
+        public string weapon = "火箭筒";
+        //布林值
+        public bool isDead = false;
+        public bool isGrounded = true;
+
+        #region Unity 常用類型
+        //向量
+        public Vector2 v2Position;
+        public Vector2 v2One = Vector2.one;
+        public Vector2 v2Custom = new Vector2(7.5f, 9.5f);
+        public Vector2 v3Custom = new Vector3(1, 2, 3);
+        public Vector2 v4Custom = new Vector4(9, 8, 7, 6);
+
+        //顏色
+        public Color colorDefult;
+        public Color colorRed = Color.red;
+        public Color colorCustom = new Color(1, 0, 1);
+        public Color colorCustomRGBA = new Color(0, 1, 0, 0.5f);
+
+        //列舉資料 按鍵
+        public KeyCode keyA = KeyCode.A;
+        public KeyCode keyJumo = KeyCode.Space;
+        public KeyCode keyFire = KeyCode.Mouse0;
+
+        //素材類型:不能指定值 透過 API 或者屬性面板拖曳
+        public AudioClip soundAttack;
+        public Sprite picureWin;
+        public Material materialDissolve;
+        
+        //遊戲物件:階層面板與專案內的物件或預置物
+        public GameObject goBlue;
+        public GameObject prefabMarble;
+       
+        //元件
+        public ParticleSystem psLight;
+        public Camera maimCamera;
+
+
+        #endregion
+
+
+        public void Awake()
+        {
+            // 取 取得資料 Get
+            // 欄位名稱
+            //以 Unity 屬性面板為主
+            print(level);
+            print("速度:" + speed);
+            //存 存放資料 Set
+        }
+
 
     }
 
