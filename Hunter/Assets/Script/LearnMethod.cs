3bf489a05b97da81e2e4662fe9b78d3b6f71deaf
hsinyingchen <43963801+hsinyingchen@users.noreply.github.com>
2022-10-08 16:55:55 +0800

    學習方法2

--- Hunter/Assets/Script/LearnMethod.cs
+++ Hunter/Assets/Script/LearnMethod.cs
@@ -24,12 +24,45 @@ namespace Chen
             //方法名稱():
             Test();
             Test();
+            //呼叫有參數的方法需要填入對應的引數
+            NumberTest(10);
+            NumberTest(999);
 
+            ShootMarble(10, "一般");
+            ShootMarble(50, "火焰");
 
+            //有預設值的參數可以不用選 會以預設值
+            ShootMarble(7);
 
+            //3, 火焰, 火球
+            ShootMarble(3, "火焰", "火球");
+
+            //9, 一般, 閃電
+            ShootMarble(9, "閃電");             //錯誤執行結果: 9,閃電,光點     
+            ShootMarble(9, effect: "閃電");
 
 		}
+        //參數語法:
+        //類型 參數 1 名稱 類型 三數 2 名稱... (沒有上限)
+        private void NumberTest(int number1) 
+        {
+            //= 指定運算子
+            //先執行指定運算子右邊 在指定結果給左邊
+            number1 = number1 + 10;
+          print("數字加10的結果:" + number1);
+        }
 
+        //參數預設值
+        //類型 參數名稱 指定 預設值
+        private void ShootMarble(int count, string type = "一般", string effect = "光點") 
+        {
+            print("彈珠數量:" + count);
+            print("彈珠屬性:" + type);
+            print("彈珠特效:" + effect);
+
+            //有預設值的參數必須寫在後面
+
+        }
 
 
 	}
